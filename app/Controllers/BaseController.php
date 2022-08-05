771bdfed2ac874dc8f9ee345ff9336089492ba71
dhonstudio <doon13@gmail.com>
2022-08-06 00:41:02 +0700

    fix fontawesome

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -148,7 +148,7 @@ abstract class BaseController extends Controller
 
             'css'   => '
                 <link rel="stylesheet" href="' . $this->assets . 'vendor/templatemo_538_digital_trend/css/bootstrap.min.css">
-                <link rel="stylesheet" href="' . $this->assets . 'vendor/templatemo_538_digital_trend/css/font-awesome.min.css">
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
                 <link rel="stylesheet" href="' . $this->assets . 'vendor/templatemo_538_digital_trend/css/aos.css">
                 <link rel="stylesheet" href="' . $this->assets . 'vendor/templatemo_538_digital_trend/css/owl.carousel.min.css">
                 <link rel="stylesheet" href="' . $this->assets . 'vendor/templatemo_538_digital_trend/css/owl.theme.default.min.css">
